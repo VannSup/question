e2df2a025b82e870607cc13257a3571125284124
Yannk <yann.kerleguer@orange.fr>
2020-12-18 12:09:05 +0100

    Update pour la compilation

--- src/main/java/fr/gamedev/question/data/Tag.java
+++ src/main/java/fr/gamedev/question/data/Tag.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Parameter;
@@ -20,9 +21,9 @@ public class Tag {
     /**
      *
      */
-    @GeneratedValue(generator = "seq_gen_answer")
-    @GenericGenerator(name = "seq_gen_answer", strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator", parameters = {
-            @Parameter(name = "sequence_name", value = "seq_answer"), @Parameter(name = "initial_value", value = "0"),
+    @GeneratedValue(generator = "seq_gen_tag")
+    @GenericGenerator(name = "seq_gen_tag", strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator", parameters = {
+            @Parameter(name = "sequence_name", value = "seq_tag"), @Parameter(name = "initial_value", value = "0"),
             @Parameter(name = "increment_size", value = "1") })
     @Id
     private long id;
@@ -40,6 +41,7 @@ public class Tag {
     /**
      *
      */
+    @ManyToMany
     private List<Question> questions;
 
     /**
--- src/main/java/fr/gamedev/question/data/enumeration/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
-     *
-     */
-package fr.gamedev.question.data.enumeration;
